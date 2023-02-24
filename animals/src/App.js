9c738ba88e6d5e7fad24f064e8b53c3c9eb8c9af
Willian Haas <haaswillian@gmail.com>
2023-02-24 12:19:19 -0800

    - finished app

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,11 +1,28 @@
+import './App.css';
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
+  const [animals, setAnimals] = useState([]);
+
   const handleClick = () => {
-    console.log('button clicked');
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
+  const renderAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
   return (
-    <div>
+    <div className="app">
       <button onClick={handleClick}>Add Animal</button>
+      <div className="animal-list">{renderAnimals}</div>
     </div>
   );
 }
