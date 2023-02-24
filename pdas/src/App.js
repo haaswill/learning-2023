10adc2368dde4fd5a7f5eae41c5d99ca163397a9
Willian Haas <haaswillian@gmail.com>
2023-02-23 16:16:45 -0800

    - Finished section: Very simple react concepts

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,13 +1,48 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App() {
   return (
     <div>
-      <div>Personal Digital Assitants</div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
 
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Alexa was created by Amazon"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+                description="Alexa was created by Microsoft"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description="Alexa was created by Apple"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
